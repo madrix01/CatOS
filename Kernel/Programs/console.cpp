f08c616daef23b0e6ad7e842c3733ced5e9313b4
madrix01 <shlokcpatel2001@gmail.com>
2021-07-01 20:12:02 +0530

    Added makefile

--- Kernel/Programs/console.cpp
+++ Kernel/Programs/console.cpp
@@ -1,6 +1,7 @@
 #pragma once 
 #include "../libs/stdlib.cpp"
 #include "../drivers/console.cpp"
+#include "../colorCodes.h"
 
 extern "C" void shutdown();
 
@@ -10,5 +11,8 @@ void programRun(void* bufPtr, int buf_size){
     }else if(memcmp(bufPtr, "exit", 4) == 0){
         write("exiting");
         shutdown();
+
+    }else{
+        write("[CatOS] Command not found", 0x0C);
     }
 }
